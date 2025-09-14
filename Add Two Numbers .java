0710cabf54c8b547d50474bead536ef09672b55e
yyh602 <s1411131031@nutc.edu.tw>
2025-09-14 23:31:27 +0800

    Update Add Two Numbers .java

--- Add Two Numbers .java
+++ Add Two Numbers .java
@@ -1,14 +1,17 @@
-//暴力破解法
+//One-pass Hash Table
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
         int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
             }
+            numMap.put(nums[i], i);
         }
+
         return new int[]{}; // No solution found
     }
 }
