3edb7064da9734e706fcc5193d7bd1930cd88e34
yyh602 <s1411131031@nutc.edu.tw>
2025-10-05 08:28:55 +0800

    Create 704.c

---
+++ Array/704. Binary Search/solution 2/704.c
@@ -0,0 +1,26 @@
+// 解法2：左閉右開
+
+// Time Complexity：O(log n)
+
+// Space Complexity：O(1)
+
+int search(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize-1;
+    int mid = 0;
+
+    while (left<right){
+        mid = (left+right)/2;
+
+        if(nums[mid]>target){
+            right = mid-1;
+        }
+        else if(nums[mid]<target){
+            left = mid+1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
