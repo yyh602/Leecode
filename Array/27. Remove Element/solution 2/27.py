9d2b9e5a2888f9b385f76801f3b4868103a2cdb5
yyh602 <s1411131031@nutc.edu.tw>
2025-10-05 23:46:34 +0800

    Update 27.py

--- Array/27. Remove Element/solution 2/27.py
+++ Array/27. Remove Element/solution 2/27.py
@@ -1,7 +1,15 @@
-# 
+# 雙指針法（快慢指針法）：透過一個快指針和慢指針在一個循環完成下兩個循環的工作。
 
+'''
+定義快慢指針
 
+快指標：尋找新的儲存元素，新的儲存就是不含目標元素的存儲
+慢指標：指向更新新記憶體下的位置
+'''
 
+# Time Complexity：O(n)
+
+# Space Complexity：O(1)
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
